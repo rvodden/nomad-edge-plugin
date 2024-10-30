8549d19b5173728f24cb696aa5bee5116cd5f05c
Richard Vodden <richard@humanisingautonomy.com>
2024-10-30 07:03:27 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package main
+
+import (
+	log "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/plugins"
+
+	"github.com/hashicorp/nomad-skeleton-device-plugin/device"
+)
+
+func main() {
+	// Serve the plugin
+	plugins.Serve(factory)
+}
+
+// factory returns a new instance of our example device plugin
+func factory(log log.Logger) interface{} {
+	return device.NewPlugin(log)
+}
